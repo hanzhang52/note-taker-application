cc6991a42c5aa9febe15792d50337e2179da1c21
hanzhang52 <hanzhang52@yahoo.com>
2022-08-30 17:55:02 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var notes = require("./Develop/db/db.json");
+var notes = require("./db/db.json");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -55,12 +55,12 @@ app.use(express.static("./public"));
 
 // Routes to HTML page
 
-app.get("/notes.html", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // Add Listener
