3dc47d00f24f2446db1359d8fadaeaf307648e2a
hanzhang52 <hanzhang52@yahoo.com>
2022-08-30 13:49:51 -0500

    updated server.js and .gitignore

---
+++ server.js
@@ -0,0 +1,83 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+var notes = require("./Develop/db/db.json");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+currentID = notes.length;
+
+// API Routes
+
+app.get("/api/notes", function (req, res) {
+  return res.json(notes);
+});
+
+app.post("/api/notes", function (req, res) {
+  var newNote = req.body;
+
+  newNote["id"] = currentID + 1;
+  currentID++;
+  console.log(newNote);
+
+  notes.push(newNote);
+
+  rewriteNotes();
+
+  return res.status(200).end();
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  res.send("Got a DELETE request at /api/notes/:id");
+
+  var id = req.params.id;
+
+  var idLess = notes.filter(function (less) {
+    return less.id < id;
+  });
+
+  var idGreater = notes.filter(function (greater) {
+    return greater.id > id;
+  });
+
+  notes = idLess.concat(idGreater);
+
+  rewriteNotes();
+});
+
+// Access files in public folder
+
+app.use(express.static("public"));
+
+// Routes to HTML page
+
+app.get("/notes.html", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Add Listener
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+
+// Functions added
+
+function rewriteNotes() {
+  fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
+    if (err) {
+      console.log("error");
+      return console.log(err);
+    }
+
+    console.log("Success!");
+  });
+}
